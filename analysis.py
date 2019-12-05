9a9e53c5b265f890f92caf667c029ffb83191362
Matej <matej.ciglenecki@gmail.com>
2019-12-05 15:37:55 +0100

    load images based on emotion

--- analysis.py
+++ analysis.py
@@ -8,27 +8,81 @@ import matplotlib.pyplot as plt
 import os
 import pathlib
 
-dataset_location = '/run/media/matej/hdd-main/1-projects/1-tech/2019-projekt/ck+'
+# ck+
+#     ├── emotions
+#     │   ├── S005
+#     │   │   └── 001
+#     │   │   │   ├── S005_001_00000011_emotion.txt
+#     │   │   │   │   └── 3.0000000e+00
+#     │   ├── S010
+#     │   │   ├── 001
+#     │   │   │   EMPTY!
+#     │   │   ├── 002
+#     │   │   │   ├── S010_002_00000014
+# _emotion.txt
+#     │   │   │   │   └── 7.0000000e+00
+#     │   │   ├── 003
+#             ...
 
-data_dir = pathlib.Path(dataset_location)
-
-print(data_dir)
-
-list_ds = tf.data.Dataset.list_files(str(data_dir/'*'))
-for f in list_ds.take(5):
-  print(f.numpy())
-# CLASS_NAMES = np.array([item.name for item in data_dir.glob('*')])cd
+#     ├── facs
+#             ├── S005
+#             ├── S010
+#             ├── S011
+#             ...
+#     ├── images
+#             ├── S005
+#             ├── S010
+#             ├── S011
+#             ...
+#     └── landmarks
+#             ├── S005
+#             ├── S010
+#             ├── S011
+#             ...
 
 # IMG_WIDTH = 640
 # IMG_HEIGHT = 490
-# for f in list_ds.take(5):
-#   print(f.numpy())
 
-# def get_label(file_path):
-#   parts = tf.strings.split(file_path, os.path.sep)
-#   # Second to last is classnames
-#   return parts[-2] == CLASS_NAMES
+AUTOTUNE = tf.data.experimental.AUTOTUNE
 
+dataset_location = '/run/media/matej/hdd-main/1-projects/1-tech/2019-projekt/ck+'
+
+data_dir = pathlib.Path(dataset_location)
+ds = list()
+
+emotions = tf.data.Dataset.list_files(str(data_dir/'emotions/*/*/*'))
+facs = tf.data.Dataset.list_files(str(data_dir/'facs/*/*/*'))
+images = tf.data.Dataset.list_files(str(data_dir/'images/*/*/*'))
+landmarks = tf.data.Dataset.list_files(str(data_dir/'landmarks/*/*/*'))
+
+data_files = tf.data.Dataset.list_files(str(data_dir/'*/*/*/*'))
+
+for file_emotion in emotions.take(5):
+    print(file_emotion)
+    print(os.path.splitext(file_emotion.numpy())[0])
+
+for file_emotion in emotions.take(5):
+    parts = tf.strings.split(file_emotion, os.path.sep)
+    file_name = parts[len(parts) - 1]
+    print(file_name)
+
+data_directories = np.array([item.name for item in data_dir.glob('*')])
+
+
+def process_path(file_path):
+    print(file_path)
+    parts = tf.strings.split(file_path, os.path.sep)
+
+    os.path.splitext(file_emotion.numpy())[0]
+    file_name = parts[len(parts) - 1].numpy()
+
+    print(file_name)
+    return file_name
+
+
+# num_parallel_calls = multiple images are processed in parallel
+labeled_ds = emotions.map(process_path, num_parallel_calls=AUTOTUNE)
+print(labeled_ds)
 
 # def decode_img(img):
 #   # string -> 3D uint8 tensor
